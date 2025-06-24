b17001f674e153a8549298c9c5087e49fcbab7ea
Adhyyan Kumar <57061577+AdhyyanKumar@users.noreply.github.com>
2025-06-24 19:00:52 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ high_chol = st.selectbox("High Cholesterol", [0, 1])
 chol_check = st.selectbox("Cholesterol Check", [0, 1])
 bmi = st.slider("BMI", 10.0, 60.0, 25.0)
 smoker = st.selectbox("Smoker", [0, 1])
+stroke = st.selectbox("Stroke History", [0, 1])
+heart_disease = st.selectbox("Heart Disease or Heart Attack", [0, 1])
 phys_activity = st.selectbox("Physically Active", [0, 1])
 fruits = st.selectbox("Eats Fruits Regularly", [0, 1])
 veggies = st.selectbox("Eats Vegetables Regularly", [0, 1])
@@ -21,8 +23,9 @@ any_healthcare = st.selectbox("Has Healthcare Access", [0, 1])
 sex = st.selectbox("Sex", [0, 1])
 age = st.slider("Age Group (1 to 13)", 1, 13, 5)
 
-input_data = np.array([[high_bp, high_chol, chol_check, bmi, smoker, phys_activity, fruits,
-                        veggies, hvy_alcohol, any_healthcare, sex, age]])
+input_data = np.array([[high_bp, high_chol, chol_check, bmi, smoker, stroke,
+                        heart_disease, phys_activity, fruits, veggies,
+                        hvy_alcohol, any_healthcare, sex, age]])
 
 if st.button("Predict"):
     prediction = model.predict(input_data)[0]
